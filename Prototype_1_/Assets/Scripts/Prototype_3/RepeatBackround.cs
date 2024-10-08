6ce580c2df915ea03a8411d41a5c97e84670e1e9
Toni-Viitala <gtovi002@edu.xamk.fi>
2024-10-08 16:23:01 +0300

    Uusi scene ja isosti muutoksia.

---
+++ Prototype_1_/Assets/Scripts/Prototype_3/RepeatBackround.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackround : MonoBehaviour
+{
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < startPosition.x - 50)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
